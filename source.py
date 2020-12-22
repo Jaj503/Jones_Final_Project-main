650171b8ee43c485e78b52726b31da41f677d288
Jaj503 <73948061+Jaj503@users.noreply.github.com>
2020-12-22 11:45:08 -0500

    Final Project

--- source.py
+++ source.py
@@ -1,41 +1,47 @@
 import numpy as np
 
-class: futureTripAnalysis:
+class futureTripAnalysis:
+
+    def __init__(self, travelTimeMatrix,productionMatrix, attractionMatrix,kij):
+        self.travelTimeMatrix = travelTimeMatrix
+        self.productionMatrix = productionMatrix
+        self.attractionMatrix = attractionMatrix
+        self.kij = kij
 
     """ The future trip analysis is seperated into 3 parts to give the user flexibility in determining which analysis is
     of focus. Trip Distribution creates matrices based on trip type. Model Split aids in analyzing the type of mode of
     transportation expected to be use and its demand. The trip table provides an overall analyzation of the number of vehicles
     expected to be use."""
 
-    def TripDistribution(traveltimeMatrix, productionmatrix, attractionmatrix, kij):
-        HBW_frictionFactor = traveltimeMatrix * -0.9982 + 101.88
-        HBO_frictionFactor = traveltimeMatrix * -0.8684 + 88.633
-        NHB_frictionFactor = traveltimeMatrix *  -0.5818 + 59.384
+    def TripDistribution(self):
+        HBW_frictionFactor = self.travelTimeMatrix * -0.9982 + 101.88
+        HBO_frictionFactor = self.travelTimeMatrix * -0.8684 + 88.633
+        NHB_frictionFactor = self.travelTimeMatrix *  -0.5818 + 59.384
         """ Above is the equation for obtaining the friction factor for the different types of trips as a function of time.
         This is needed in solving for the trip Matrix using the equation Pi * (AjFijKij) / sum(AjFijKij). """
 
-        HBW_AjFijKij = HBW_frictionFactor * attractionMatrix[:,0] * kij
-        HBO_AjFijKij = HBO_frictionFactor * attractionMatrix[:,1] * kij
-        NHB_AjFijKij = NHB_frictionFactor * attractionMatrix[:,2] * kij
-         """ Above is used for solving for AjFiJKij part of the equation """
+        HBW_AjFijKij = HBW_frictionFactor * self.attractionMatrix[:,0] * self.kij
+        HBO_AjFijKij = HBO_frictionFactor * self.attractionMatrix[:,1] * self.kij
+        NHB_AjFijKij = NHB_frictionFactor * self.attractionMatrix[:,2] * self.kij
+        """ Above is used for solving for AjFiJKij part of the equation """
 
         HBW_sumAjFijKij = np.sum(HBW_AjFijKij,axis=1)
         HBO_sumAjFijKij = np.sum(HBO_AjFijKij,axis=1)
         NHB_sumAjFijKij = np.sum(NHB_AjFijKij,axis=1)
         """ Above is used for solving the sum of the AjFijKij"""
 
-        HBW_tripMatrix = HBW_AjFijKij.transpose() * productionMatrix[:,0] / HBW_sumAjFijKij
+        HBW_tripMatrix = HBW_AjFijKij.transpose() * self.productionMatrix[:,0] / HBW_sumAjFijKij
         HBW_tripMatrix = HBW_tripMatrix.round().transpose()
-        HBO_tripMatrix = HBO_AjFijKij.transpose() * productionMatrix[:,1] / HBO_sumAjFijKij
+        HBO_tripMatrix = HBO_AjFijKij.transpose() * self.productionMatrix[:,1] / HBO_sumAjFijKij
         HBO_tripMatrix = HBO_tripMatrix.round().transpose()
-        NHB_tripMatrix = NHB_AjFijKij.transpose() * productionMatrix[:,2] / NHB_sumAjFijKij
+        NHB_tripMatrix = NHB_AjFijKij.transpose() * self.productionMatrix[:,2] / NHB_sumAjFijKij
         NHB_tripMatrix = NHB_tripMatrix.round().transpose()
         """ Above is the final calculation for the trip matrix of the three types of trips """
 
-        return HBW_tripMatrix, HBO_tripMatrix, NHB_tripMatrix
+        return [HBW_tripMatrix, HBO_tripMatrix, NHB_tripMatrix]
         """ The result is 3 matrices of the person-trip of each zone for each trip type """
 
-    def ModelSplit(HBW_TripMatrix, HBO_TripMatrix, NHB_TripMatrix, Auto_travelTime, Bus_travelTime):
+    def ModelSplit(self, tripDistribution, Auto_travelTime, Bus_travelTime):
 
         Auto_Ta, Bus_Ta = [5, 10]
         Auto_Tw, Bus_Tw = [0, 10]
@@ -55,30 +61,29 @@ class: futureTripAnalysis:
         Bus_Prob = np.exp(Bus_uFactor) / (np.exp(Auto_uFactor) + np.exp(Bus_uFactor))
         """ Above is used for finding the probability of people choosing auto or bus for each zone """
 
-        HBW_tripAutoMatrix = Auto_Prob * HBW_TripMatrix
+        HBW_tripAutoMatrix = Auto_Prob * tripDistribution[0]
         HBW_tripAutoMatrix = HBW_tripAutoMatrix.round()
-        HBW_tripBusMatrix = Bus_Prob * HBW_TripMatrix
+        HBW_tripBusMatrix = Bus_Prob * tripDistribution[0]
         HBW_tripBusMatrix = HBW_tripBusMatrix.round()
-        HBO_tripAutoMatrix = Auto_Prob * HBO_TripMatrix
+        HBO_tripAutoMatrix = Auto_Prob * tripDistribution[1]
         HBO_tripAutoMatrix = HBO_tripAutoMatrix.round()
-        HBO_tripBusMatrix = Bus_Prob * HBO_TripMatrix
+        HBO_tripBusMatrix = Bus_Prob * tripDistribution[1]
         HBO_tripBusMatrix = HBO_tripBusMatrix.round()
-        NHB_tripAutoMatrix = Auto_Prob * NHB_TripMatrix
+        NHB_tripAutoMatrix = Auto_Prob * tripDistribution[2]
         NHB_tripAutoMatrix = NHB_tripAutoMatrix.round()
-        NHB_tripBusMatrix = Bus_Prob * NHB_TripMatrix
+        NHB_tripBusMatrix = Bus_Prob * tripDistribution[2]
         NHB_tripBusMatrix = NHB_tripBusMatrix.round()
-        """ Above is used for finding the final model split analysis for each trip type """
+        """Above is used for finding the final model split analysis for each trip type """
 
-        return HBW_tripAutoMatrix, HBW_tripBusMatrix, HBO_tripAutoMatrix, HBO_tripBusMatrix, NHB_tripAutoMatrix, NHB_tripBusMatrix
+        return [HBW_tripAutoMatrix, HBW_tripBusMatrix, HBO_tripAutoMatrix, HBO_tripBusMatrix, NHB_tripAutoMatrix, NHB_tripBusMatrix]
         """ The function results in 6 matrices, a person-trip matrix for bus and for auto for each trip type """
 
-    def TripTablecreator(HBW_tripAutoMatrix, HBW_tripBusMatrix, HBO_tripAutoMatrix, HBO_tripBusMatrix, NHB_tripAutoMatrix, NHB_tripBusMatrix, HBW_Auto_averageOcupancy, HBO_Auto_averageOcupancy, NHB_Auto_averageOcupancy, Bus_averageOcupancy, peakHourFactor):
-
-        HBW_tripTable = HBW_tripAutoMatrix / HBW_Auto_averageOcupancy + HBW_tripBusMatrix / Bus_averageOcupancy
+    def TripTablecreator(self, modelSplittables, HBW_Auto_averageOcupancy, HBO_Auto_averageOcupancy, NHB_Auto_averageOcupancy, Bus_averageOcupancy, peakHourFactor):
+        HBW_tripTable = modelSplittables[0] / HBW_Auto_averageOcupancy + modelSplittables[1] / Bus_averageOcupancy
         HBW_tripTable = np.floor(HBW_tripTable)
-        HBO_tripTable = HBO_tripAutoMatrix / HBO_Auto_averageOcupancy + HBO_tripBusMatrix / Bus_averageOcupancy
+        HBO_tripTable = modelSplittables[2] / HBO_Auto_averageOcupancy + modelSplittables[3] / Bus_averageOcupancy
         HBO_tripTable = np.floor(HBO_tripTable)
-        NHB_tripTable = NHB_tripAutoMatrix / NHB_Auto_averageOcupancy + NHB_tripBusMatrix / Bus_averageOcupancy
+        NHB_tripTable = modelSplittables[4] / NHB_Auto_averageOcupancy + modelSplittables[5] / Bus_averageOcupancy
         NHB_tripTable = np.floor(NHB_tripTable)
         """ Above is used for finding the number of vehicles expected to be used for each type of trip """
 
@@ -87,7 +92,7 @@ class: futureTripAnalysis:
 
         peakHourTripTable = TripTable * peakHourFactor
         peakHourTripTable = peakHourTripTable.round()
-        """ Above is used for finding the expected demand during the peak hour or peak of unit of measurement """"
+        """ Above is used for finding the expected demand during the peak hour or peak of unit of measurement """
 
-        return TripTable, peakHourTripTable
+        return [TripTable, peakHourTripTable]
         """ The function results in a general total trip table and a peak time trip table """
--- test.py
+++ test.py
@@ -1,18 +1,46 @@
 from source import futureTripAnalysis
+import numpy as np
 
 productionMatrix = np.array([[5549, 13888, 5991],[7317, 18998, 7825],[6150, 16355, 6533],[8359, 23587, 9682],[4708, 11773, 5053]])
 attractionMatrix = np.array([[5048, 10409, 6179],[11443, 23688, 14142],[10433, 17385, 10273],[7236, 19356, 11282],[2861, 8155, 4961]])
 travelTimeMatrix = np.array([[7,9.14,18.08,20.16,33.56],[9.21,5,6.68,11.23,24.63],[18.74,6.74,15,13.56,30.96],[20.56,11.56,13.7,12,24.34],[32.22,23.22,29.42,22.64,10]])
 """ The given matrices used in prior project """
 
-HBW_tripmatriX, HBO_tripmatriX, NHB_tripmatriX = TripMatrixcreator(travelTimeMatrix, productionMatrix, attractionMatrix, 1)
+future_trip_analysis = futureTripAnalysis(travelTimeMatrix, productionMatrix, attractionMatrix, 1)
+""" The initization of the matricies in the class """
+
+tripDistributionTables = future_trip_analysis.TripDistribution()
+""" The creation of the matrices for the trip distribution """
+
+"""
+tripDistributionTables[0] = HBW Trip Table
+tripDistributionTables[1] = HBO Trip Table
+tripDistributionTables[2] = NHB Trip Table
+"""
 
 Bus_travelTimesaving = np.array([[0.09,0.08,0.14,0.30,0.27],[0.07,0.19,0.17,0.13,0.09],[0.22,0.30,0.26,0.20,0.24],[0.13,0.09,0.30,0.19,0.16],[0.22,0.18,0.27,0.29,0.21]])
 Bus_travelTimeMatrix = (1 - Bus_travelTimesaving) * travelTimeMatrix
 """ A function for creating the travel time matrix for bus. This is usually found and calculated beforehand."""
 
-HBW_tripAutoMatriX, HBW_tripBusMatriX, HBO_tripAutoMatriX, HBO_tripBusMatriX, NHB_tripAutoMatriX, NHB_tripBusMatriX = ModelSplit(HBW_tripmatriX, HBO_tripmatriX, NHB_tripmatriX, travelTimeMatrix, Bus_travelTimeMatrix)
+modelSplitTables = future_trip_analysis.ModelSplit(tripDistributionTables, travelTimeMatrix, Bus_travelTimeMatrix)
+""" The creation of the matrices for the model split """
 
-finalTrip,finalPeakTrip = TripTablecreator(HBW_tripAutoMatriX, HBW_tripBusMatriX, HBO_tripAutoMatriX, HBO_tripBusMatriX, NHB_tripAutoMatriX, NHB_tripBusMatriX, 1.1, 1.15, 1.25, 35, 0.4)
-print(finalTrip)
-print(finalPeakTrip)
+'''
+modelSplitTables[0] = HBWtrip Auto MatriX
+modelSplitTables[1] = HBWtrip Bus MatriX
+modelSplitTables[2] = HBOtrip Auto MatriX
+modelSplitTables[3] = HBOtrip Bus MatriX
+modelSplitTables[4] = NHBtrip Auto MatriX
+modelSplitTables[5] = NHBtrip Bus MatriX
+'''
+
+tripTable = future_trip_analysis.TripTablecreator(modelSplitTables, 1.1, 1.15, 1.25, 35, 0.4)
+""" The creation of the final trip tables and peak hour trip tables """
+
+"""
+tripTable[0] = Final trip table
+tripTable[1] = Peak trip table
+"""
+
+print("this is the final trip:", tripTable[0])
+print("this is the final peak trip:", tripTable[1])
